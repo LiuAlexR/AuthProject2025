f715310899221f6a34882bbc83ec632bb7b6f294
TheSilentIce <srikaryadlapati@gmail.com>
2025-07-01 10:45:51 -0600

    Basic Server and database set up

---
+++ java_backend/website_backend/src/main/java/com/example/website_backend/models/User.java
@@ -0,0 +1,25 @@
+package com.example.website_backend.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+
+@Document(collection = "users")
+@Getter
+@Setter
+@ToString
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String[] usersThatCanWatch;
+    private String[] usersToWatch;
+    private double[] currentLocation;
+    private ArrayList<double[]> path;
+    private ArrayList<double[]> pins;
+
+}
