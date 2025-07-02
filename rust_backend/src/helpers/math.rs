af2020ce5c466a975e117085f53472e5ee8a2286
Alex Liu <alexliu3000@gmail.com>
2025-07-01 21:42:57 -0600

    Update that might break things (I commented out something in tsconfig.app.json because compiler was erroring)

--- rust_backend/src/helpers/math.rs
+++ rust_backend/src/helpers/math.rs
@@ -3,18 +3,29 @@ use mongodb::{
 	Client,
 	Collection 
 };
-
+use bcrypt;
 pub async fn more_test() -> mongodb::error::Result<Option<Document>> {
-    // Replace the placeholder with your Atlas connection string
     let uri = "mongodb://localhost:27017/";
     // Create a new client and connect to the server
     let client = Client::with_uri_str(uri).await?;
-    // Get a handle on the movies collection
-    let database = client.database("TestDatabase");
-    let my_coll: Collection<Document> = database.collection("airbnb");
-    // Find a movie based on the title value
-    let my_movie = my_coll.find_one(doc! { "beds": 4 }).await?;
-    // Print the document
-    println!("Found a movie:\n{:#?}", my_movie);
-    Ok(my_movie)
+    let database = client.database("authproject2025");
+    let my_coll: Collection<Document> = database.collection("authentication");
+    let found = my_coll.find_one(doc! { "user_id": 1 }).await?;
+    return Ok(found);
+}
+pub fn hash(password: &str) -> String {
+    //Hashes the password
+    let the_hash = bcrypt::hash(password, 12);
+    match the_hash {
+        Ok(t) => return t,
+        Err(_e) => (return "error".to_string()),
+
+    }
+}
+pub fn verify_password(password: &str, hashed_password: &str) -> bool {
+    let verified = bcrypt::verify(password, hashed_password);
+    match verified {
+        Ok(t) => return t,
+        Err(_) => return false,
+    }
 }
--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,21 +1,17 @@
-/*use lib::tester::printtest::test;
+/*
 fn main() {
     test();
 }*/
 
-use mongodb::{ 
-	bson::{Document, doc},
-	Client,
-	Collection 
-};
+// use lib::tester::printtest::test;
+use lib::helpers::math;
 #[tokio::main]
-async fn main() -> mongodb::error::Result<()> {
-    let uri = "mongodb://localhost:27017/";
-    // Create a new client and connect to the server
-    let client = Client::with_uri_str(uri).await?;
-    let database = client.database("TestDatabase");
-    let my_coll: Collection<Document> = database.collection("airbnb");
-    let bed = my_coll.find_one(doc! { "beds": 4 }).await?;
-    println!("Found a bed:\n{:#?}", bed);
-    Ok(())
+async fn main() {
+    // test().await;
+    let password: &str = "1234";
+    let hash: String = math::hash(password);
+    println!("{}", hash);
+    let verified = math::verify_password("abc", &hash);
+    println!("{}", verified);
+
 }
