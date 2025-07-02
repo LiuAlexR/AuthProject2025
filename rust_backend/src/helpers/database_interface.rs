e5a7e10e47a3a1726fc171e1ba453dc96d75ac4c
LiuAlexR <alexliu3000@gmail.com>
2025-07-02 17:28:51 -0600

    Add max id finder

--- rust_backend/src/helpers/database_interface.rs
+++ rust_backend/src/helpers/database_interface.rs
@@ -3,10 +3,10 @@ use mongodb::{
     Client, Collection,
     bson::{Document, doc},
 };
+const URI: &str = "mongodb://localhost:27017/";
 pub async fn get_user_password(username: &str) -> mongodb::error::Result<Option<Document>> {
-    let uri = "mongodb://localhost:27017/";
     // Create a new client and connect to the server
-    let client = Client::with_uri_str(uri).await?;
+    let client = Client::with_uri_str(URI).await?;
     let database = client.database("authproject2025");
     let my_coll: Collection<Document> = database.collection("authentication");
     let found = my_coll.find_one(doc! { "username": username }).await?;
@@ -15,9 +15,8 @@ pub async fn get_user_password(username: &str) -> mongodb::error::Result<Option<
 pub async fn update_password(username: &str, password: &str) -> Result<mongodb::results::UpdateResult, mongodb::error::Error> {
     //Hashes the password. The input password should be in plaintext
     let pass_hash = hash(password);
-    let uri = "mongodb://localhost:27017/";
     // Create a new client and connect to the server
-    let client = Client::with_uri_str(uri).await?;
+    let client = Client::with_uri_str(URI).await?;
     let database = client.database("authproject2025");
     let my_coll: Collection<Document> = database.collection("authentication");
     // Selects the username
@@ -38,5 +37,23 @@ pub async fn verify_password_from_database(username: &str, password: &str) -> bo
         },
         Ok(None) => return false,
         Err(_) => return false,
+    }
+}
+pub async fn get_max_id() -> Result<i32, mongodb::error::Error> {
+    // Create a new client and connect to the server
+    let client = Client::with_uri_str(URI).await?;
+    let database = client.database("authproject2025");
+    let my_coll: Collection<Document> = database.collection("authentication");
+    //sorts to find the current maximum id. This is necessary because ids are going to be in sequential order
+    // Online says compiler optimizes this sort. IDK if it even works because my current test database only has one entry
+    let found = my_coll.find_one(doc! {}).sort(doc! {"user_id": -1}).await?;
+    match found {
+        Some(found_document) => {
+            match found_document.get_i32("user_id") {
+                Ok(id) => return Ok(id),
+                Err(_) => return Ok(0),
+            }
+        },
+        None => return Ok(0),
     }
 }
--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,22 +1,12 @@
-/*
-fn main() {
-    test();
-}*/
-
-use core::error;
-use std::result;
-
-// use lib::tester::printtest::test;
-use lib::helpers::{database_interface, math};
+use lib::helpers::database_interface::{self, get_max_id};
 #[tokio::main]
 async fn main() {
-    // test().await;
-    // 
-    // let hash: String = math::hash(password);
-    // println!("{}", hash);
-    // let verified = math::verify_password("abc", &hash);
-    // println!("{}", verified);
-    let username = "Bob";
+    let username = "Bobb";
     let password: &str = "12345";
-    println!("{}", database_interface::verify_password_from_database(username, password).await)
+    // println!("{}", database_interface::verify_password_from_database(username, password).await)
+    let max_id = get_max_id().await;
+    match max_id {
+        Ok(item) => println!("{}", item),
+        Err(_) => print!("error"),
+    }
 }
