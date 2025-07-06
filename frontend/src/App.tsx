73b9e3a309c85473abba6761b925aa2d111274a2
Alex Liu <alexliu3000@gmail.com>
2025-07-05 20:08:04 -0600

    And also f mongorepository If I don't understand it then it;s bad

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
+import "leaflet-defaulticon-compatibility";
 function App() {
   const [count, setCount] = useState(0)
-
+  const [latitute, setLatitude] = useState(-0.09)
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, latitute]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setLatitude((latitute) => latitute + 0.00001)}>
+        latitute is {latitute}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
